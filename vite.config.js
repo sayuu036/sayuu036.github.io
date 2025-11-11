f770e685046011ae496e1662aa1b0723c45a6786
sayuu036 <207896187+sayuu036@users.noreply.github.com>
2025-11-11 12:45:26 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                sub: resolve(__dirname, "test/index.html")
+                sub: resolve(__dirname, "test/index.html"),
+                sub2: resolve(__dirname, "page_test/index.html")
             }
         }
     }
