14af2386ac1ec4d2a055db338ac0dca1df3583a3
sayuu036 <207896187+sayuu036@users.noreply.github.com>
2025-11-11 12:43:49 +0900

    .

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import {resolve} from "path";
+
+export default defineConfig({
+    server: {
+        port: 3000,
+        open: true
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                sub: resolve(__dirname, "test/index.html")
+            }
+        }
+    }
+})
